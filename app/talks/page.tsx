9528b1a049b51174a7628e0d460d2e321c93f894
LaijuKamang <tkjheans@gmail.com>
2025-07-12 09:44:24 +0800

    Update page.tsx
    EDIT

--- app/talks/page.tsx
+++ app/talks/page.tsx
@@ -5,7 +5,7 @@ export default function Talks() {
         <h1 className="text-3xl font-bold mb-4">🎤 Talks</h1>
         <ul className="space-y-4">
           <li>
-            <h2 className="text-xl font-semibold">None..</h2>
+            <h2 className="text-xl font-semibold">BELUM ADA</h2>
             <p className="text-gray-600 dark:text-gray-400">None.</p>
           </li>
         </ul>
