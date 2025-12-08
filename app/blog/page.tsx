5d76ee1ac389cd5f637eb8258669163013dc0859
Hen Umbu Laiya Sobang <tkjheans@gmail.com>
2025-12-08 23:53:34 +0800

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ export default function Blog() {
     <main className="min-h-screen bg-white dark:bg-black text-gray-900 dark:text-gray-100 px-6 py-12">
       <section className="max-w-3xl mx-auto">
         <h1 className="text-3xl font-bold mb-4">📝 Blog</h1>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">None.</p>
+        <p className="text-gray-600 dark:text-gray-400 mb-4">😂😂😂😂😂😂</p>
         <ul className="space-y-4">
           <li>
             <h2 className="text-xl font-semibold">Haii Ayu Sayang</h2>
